0ad878e8a489798d1b555ca5e54d1971df1d18a8
Alyona <borisovsk_a@mail.ru>
2023-01-24 11:16:57 +0300

    feat: Incorrect way working (ANR)

--- app/src/main/java/com/example/process/IncorrectFragment.kt
+++ app/src/main/java/com/example/process/IncorrectFragment.kt
@@ -1,6 +1,7 @@
 package com.example.process
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -28,7 +29,17 @@ class IncorrectFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.startBtn.setOnClickListener {
-            binding.enterNameTv
+            val time = binding.enterTime.text.toString().toInt()
+
+            for (i in time downTo 0) {
+                try {
+                    Log.d(TAG, "Timer updated: $i")
+                    binding.resultTv.text = i.toString()
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                    Log.e(TAG, "$e.stackTrace")
+                }
+            }
         }
     }
 
@@ -38,6 +49,7 @@ class IncorrectFragment : Fragment() {
     }
 
     companion object {
+        const val TAG = "IncorFragLog"
         @JvmStatic
         fun newInstance() =
             IncorrectFragment()
